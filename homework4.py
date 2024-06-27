ba55a528eaf59273a8fe9f181814fcfca96aa17b
IlonaWishec <ilonawishec.management@gmail.com>
2024-06-27 18:44:28 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_sring = input() #My Dog
+print(len(my_sring)) #6
+print(my_sring.upper()) #MY DOG
+print(my_sring.lower()) #my dog
+print(my_sring.replace(' ', '')) #MyDog
+print(my_sring [0]) #M
+print(my_sring [-1]) #g
